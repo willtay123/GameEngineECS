21836ab68ccd004d9f6df72e57d735387c1e4b8d
William Taylor <willtay123@hotmail.co.uk>
2020-02-15 21:05:36 +0000

    Changed SystemManger to be able to return a list of its sytems Added a System implementation to Test Proj Added some test methods to SystemManagerTest

--- src/Code/EngineUnitTests/Managers/SystemManagerTest.cpp
+++ src/Code/EngineUnitTests/Managers/SystemManagerTest.cpp
@@ -1,7 +1,9 @@
+#pragma once
+
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "Managers/SystemManager.h"
-#include "Interfaces/ISystem.h"
+#include "TestObjects/Implementations/System.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace EngineECS;
@@ -11,8 +13,20 @@ namespace EngineUnitTests
 	TEST_CLASS(SystemManagerTest) {
 	public:
 
-		TEST_METHOD(AddingASystem) {
-			
+		TEST_METHOD(AddingAnUpdateSystem) {
+			System* system = new System();
+			SystemManager::AddUpdateSystem(system);
+
+			const vector<ISystem*>* systems = SystemManager::GetUpdateSystems();
+			Assert::IsNotNull((*systems)[0]);
+		}
+
+		TEST_METHOD(AddingARenderSystem) {
+			System* system = new System();
+			SystemManager::AddRenderSystem(system);
+
+			const vector<ISystem*>* systems = SystemManager::GetRenderSystems();
+			Assert::IsNotNull((*systems)[0]);
 		}
 	};
 }
---
+++ src/Code/EngineUnitTests/TestObjects/Implementations/System.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "System.h"
+
+const string* System::GetName() {
+	return nullptr;
+}
+
+void System::GiveEntities(vector<Entity*>* entityList) {
+}
+
+void System::OnAction(double deltaTime) {
+}
