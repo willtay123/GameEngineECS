d1c44db5b8a33e80e3480b41adb2d57eb5b1eaec
William Taylor <willtay123@hotmail.co.uk>
2020-02-18 14:17:35 +0000

    Fixed the use of the new camera object

--- src/Code/Game/Scenes/EndScene.cpp
+++ src/Code/Game/Scenes/EndScene.cpp
@@ -12,9 +12,9 @@ EndScene::~EndScene() {
 
 void EndScene::Initialise() {
 	_camera = new Camera();
-	_camera->MoveTo(&vec3(0, 10, 0));
-	_camera->LookPosition(&vec3(0, 0, 0));
-	_camera->SetUp(&vec3(0, 0, 1));
+	_camera->SetPosition(vec3(0, 10, 0));
+	_camera->LookAt(vec3(0, 0, 0), vec3(0,1,0));
+	_camera->SetUp(vec3(0, 0, 1));
 
 	// IO
 	RendererGL* rend = (RendererGL*)RenderManager::_renderer;
