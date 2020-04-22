a8fbe9b0ad3529e0feafeeef5977b4f4cc72ef67
Will <47634552+willtay123@users.noreply.github.com>
2020-04-22 18:23:46 +0100

    Added more tests for the camera

--- src/Code/EngineUnitTests/Objects/CameraTest.cpp
+++ src/Code/EngineUnitTests/Objects/CameraTest.cpp
@@ -73,15 +73,67 @@ namespace EngineUnitTests
 				);
 			}
 
-			//TEST_METHOD(GetView)
+			TEST_METHOD(GetView) {
+				Camera camera;
 
-			//TEST_METHOD(GetProjection)
+				camera.LookAt(vec3(0, 0, 1), vec3(0, 1, 0));
+				camera.Update(0.0f);
+				mat4 view = camera.GetView();
+				mat4 compare = glm::lookAt(vec3(0,0,0), vec3(0,0,1), vec3(0,1,0));
 
-			//TEST_METHOD(Rotate90DegreesLeft)
+				Assert::IsTrue(view == compare, L"failed to get the view matrix");
+			}
 
-			//TEST_METHOD(Rotate90DegreesRight)
+			TEST_METHOD(GetProjection) {
+				Camera camera;
 
-			//TEST_METHOD(LookDown)
+				mat4 proj = camera.GetProjection();
+
+				mat4 projection = glm::perspective(
+					(float)glm::radians(80.0f),
+					1280.0f / 780.0f,
+					1.0f,
+					50.0f
+				);
+
+				Assert::IsTrue(proj == projection, L"Failed to get the projection matrix");
+			}
+
+			TEST_METHOD(Rotate90DegreesLeft) {
+				Camera camera;
+
+				camera.LookAt(vec3(0, 0, 1), vec3(0, 1, 0));
+				camera.TurnOnAxis(-90, vec3(0, 1, 0));
+
+				mat4 newLook = camera.GetView();
+				mat4 compare = glm::lookAt(vec3(0, 0, 0), vec3(-1, 0, 0), vec3(0, 1, 0));
+
+				Assert::IsTrue(newLook == compare, L"Failed to rotate 90 degrees left");
+			}
+
+			TEST_METHOD(Rotate90DegreesRight) {
+				Camera camera;
+
+				camera.LookAt(vec3(0, 0, 1), vec3(0, 1, 0));
+				camera.TurnOnAxis(90, vec3(0, 1, 0));
+
+				mat4 newLook = camera.GetView();
+				mat4 compare = glm::lookAt(vec3(0, 0, 0), vec3(1, 0, 0), vec3(0, 1, 0));
+
+				Assert::IsTrue(newLook == compare, L"Failed to rotate 90 degrees left");
+			}
+
+			TEST_METHOD(LookDown) {
+				Camera camera;
+
+				camera.LookAt(vec3(0, 0, 1), vec3(0, 1, 0));
+				camera.TurnOnAxis(90, vec3(1, 0, 0));
+
+				mat4 newLook = camera.GetView();
+				mat4 compare = glm::lookAt(vec3(0, 0, 0), vec3(0, -1, 0), vec3(0, 0, 1));
+
+				Assert::IsTrue(newLook == compare, L"Failed to rotate 90 degrees left");
+			}
 		};
 	}
 }
